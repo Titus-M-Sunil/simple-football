003f7812119987f36f4ee11ae583c553eb026dda
Titus M. Sunil <titus.cvv241164@cvv.ac.in>
2025-08-03 22:45:59 +0530

    Added foot/ball function

--- simple-football.py
+++ simple-football.py
@@ -3,16 +3,37 @@ import random
 # Number of times player/computer has passed the ball
 passCount = 0
 
-# Asks for user input to decide if they choose odd/even
+# User input to choose odd/even
 userOddEven = input('Choose odd/even: ')
 print('You chose', userOddEven)
 
-# Asks for number input to decide if the outcome is odd/even
+# Number input (from 1 to 4) to decide if the outcome is odd/even
 userInput = int(input('\nEnter a number (1-4): '))
 
-# Computer number input is chosen randomly
+# Computer number input (from 1 to 4) is chosen randomly
 computerInput = random.randint(1, 4)
 print('Computer chose', computerInput)
 
-# Calculation to check is sum of computer and player input is odd/even
+# Calculation to check if sum of computer and player input is odd/even
 calculation = (computerInput + userInput) % 2
+
+def footOrBallSelection():
+    if (calculation == 0): # even
+        print('Outcome was even')
+        if (userOddEven == 'even'):
+            userFootBall = input('You get to play first. Choose (foot/ball): ')
+            # print(userFootBall)
+            return userFootBall
+        elif (userOddEven == 'odd'):
+            print('Computer plays first.')
+            return 'BALL'
+        
+    elif (calculation != 0): # odd
+        print('Outcome was odd')
+        if (userOddEven == 'even'):
+            print('Computer plays first.')
+            return 'BALL'
+        elif (userOddEven == 'odd'):
+            userFootBall = input('You get to play first. Choose (foot/ball): ')
+            # print(userFootBall)
+            return userFootBall
